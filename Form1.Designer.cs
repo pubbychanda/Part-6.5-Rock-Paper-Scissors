2cec837a3bf02453c2ca269127a28e223d919fcb
Nolan Neilson <NeilNola532@GoTVDSB.ca>
2021-03-04 12:54:34 -0500

    need to code instructions form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,6 +46,7 @@
             this.lblLoses = new System.Windows.Forms.Label();
             this.lblTies = new System.Windows.Forms.Label();
             this.btnReset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.grpPlayerChoice.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).BeginInit();
@@ -175,7 +176,7 @@
             this.grpScoreBoard.Controls.Add(this.lblLoses);
             this.grpScoreBoard.Controls.Add(this.lblWins);
             this.grpScoreBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grpScoreBoard.Location = new System.Drawing.Point(328, 227);
+            this.grpScoreBoard.Location = new System.Drawing.Point(330, 227);
             this.grpScoreBoard.Name = "grpScoreBoard";
             this.grpScoreBoard.Size = new System.Drawing.Size(97, 65);
             this.grpScoreBoard.TabIndex = 7;
@@ -224,11 +225,22 @@
             this.btnReset.UseVisualStyleBackColor = false;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(330, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Instructions!";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(439, 302);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.grpScoreBoard);
             this.Controls.Add(this.lblCountdown);
@@ -270,6 +282,7 @@
         private System.Windows.Forms.Label lblLoses;
         private System.Windows.Forms.Label lblWins;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button button1;
     }
 }
 
